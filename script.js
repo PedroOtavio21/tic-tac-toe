9074b63a527f81ca574dfbfa24fa5636cf74dbb4
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2024-08-26 23:15:57 -0300

    Adiciona funções para verificação de jogador vencedor e suas respectivas funções auxiliares.

--- script.js
+++ script.js
@@ -18,9 +18,38 @@ function initializeGame(){
     })
 }
 
+function getWinRegions() {
+    const winRegions = []
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[0][1] && vBoard[0][0] === vBoard[0][2])
+      winRegions.push("0.0", "0.1", "0.2")
+    if (vBoard[1][0] && vBoard[1][0] === vBoard[1][1] && vBoard[1][0] === vBoard[1][2])
+      winRegions.push("1.0", "1.1", "1.2")
+    if (vBoard[2][0] && vBoard[2][0] === vBoard[2][1] && vBoard[2][0] === vBoard[2][2])
+      winRegions.push("2.0", "2.1", "2.2")
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[1][0] && vBoard[0][0] === vBoard[2][0])
+      winRegions.push("0.0", "1.0", "2.0")
+    if (vBoard[0][1] && vBoard[0][1] === vBoard[1][1] && vBoard[0][1] === vBoard[2][1])
+      winRegions.push("0.1", "1.1", "2.1")
+    if (vBoard[0][2] && vBoard[0][2] === vBoard[1][2] && vBoard[0][2] === vBoard[2][2])
+      winRegions.push("0.2", "1.2", "2.2")
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[1][1] && vBoard[0][0] === vBoard[2][2])
+      winRegions.push("0.0", "1.1", "2.2")
+    if (vBoard[0][2] && vBoard[0][2] === vBoard[1][1] && vBoard[0][2] === vBoard[2][0])
+      winRegions.push("0.2", "1.1", "2.0")
+    return winRegions
+  }
+
 function updateTitle(){
     const playerInput = document.getElementById(turnPlayer)
     document.getElementById('turn-player').innerText = playerInput.value
+}
+
+function handleWin(regions){
+    regions.forEach((region) => {
+        document.querySelector(`[data-region="${region}"]`).classList('winner')
+    })
+    const playerName = document.getElementById('turn-player').value
+    document.querySelector('h2').innerHTML = `${playerName} venceu!`
 }
 
 function handleBoardClick(ev){
@@ -33,12 +62,27 @@ function handleBoardClick(ev){
         ev.currentTarget.innerText = 'X'
         vBoard[row][column] = 'X'
     } else {
-        ev.currentTarget.innerText = 'X'
-        vBoard[row][column] = 'X'
+        ev.currentTarget.innerText = 'O'
+        vBoard[row][column] = 'O'
     }
 
     console.clear()
     console.table(vBoard)
+    disableRegion(ev.currentTarget)
+    const winRegions = getWinRegions()
+    if(winRegions.length > 0){
+        handleWin(winRegions)
+    } else if (vBoard.flat().includes('')){
+        turnPlayer = turnPlayer === 'player1' ? 'player2' : 'player1'
+        updateTitle()
+    } else {
+        document.querySelector('h2').innerHTML = 'Empate!'
+    }
+}
+
+function disableRegion(element){
+    element.style.cursor = 'default'
+    element.removeEventListener('click', handleBoardClick)
 }
 
 startBtn.addEventListener('click', initializeGame)
